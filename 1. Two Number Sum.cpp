2b1feda2a2d844e641760aa17c4280e6d4adf7ce
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-12-01 12:51:03 +0000

    Add files via upload

---
+++ 1. Two Number Sum.cpp
@@ -0,0 +1,28 @@
+// Vineet Kumar
+
+// Solution One TIME: O(N^2) , SPACE: O(1)
+std::vector<int> twoNumberSum(std::vector<int> array, int targetSum)
+{
+	for (int i = 0; i < array.size(); i++)
+		for (int j = i+1; j < array.size(); j++)
+			if (array[i] + array[j] == targetSum)
+				return {array[i],array[j]};
+	return {};
+}
+
+// Solution Two TIME: O(N.log(N)) , SPACE: O(1)
+std::vector<int> twoNumberSum(std::vector<int> array, int targetSum)
+{
+	sort(array.begin(), array.end());
+	int left = 0, right = array.size() - 1;
+	while (left < right)
+	{
+		if ((array[left] + array[right]) == targetSum)
+			return { array[left],array[right] };
+		else if ((array[left] + array[right]) < targetSum)
+			left++;
+		else if ((array[left] + array[right]) > targetSum)
+			right--;
+	}
+	return {};
+}
---
+++ 3. Validate Subsequence.cpp
@@ -0,0 +1,14 @@
+// TIME: O(N) , SPACE: O(1)
+
+bool isValidSubsequence(std::vector<int> array, std::vector<int> sequence)
+{
+	int arridx=0,seqidx=0;
+	while(arridx < array.size() && seqidx < sequence.size())
+	{
+		if(array[arridx] == sequence[seqidx])
+			seqidx++;
+		arridx++;
+	}
+	
+	return seqidx == sequence.size();
+}

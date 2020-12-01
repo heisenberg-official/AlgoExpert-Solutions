1b7b66fc7e8757a956860c7c8a5743d104b34189
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-12-01 13:56:56 +0530

    Update 1. Two Number Sum.cpp

--- EASY SET/1. Two Number Sum.cpp
+++ EASY SET/1. Two Number Sum.cpp
@@ -9,3 +9,20 @@ std::vector<int> twoNumberSum(std::vector<int> array, int targetSum)
 				return {array[i],array[j]};
 	return {};
 }
+
+// Solution Two TIME: O(N.log(N)) , SPACE: O(1)
+std::vector<int> twoNumberSum(std::vector<int> array, int targetSum)
+{
+	sort(array.begin(), array.end());
+	int left = 0, right = array.size() - 1;
+	while (left < right)
+	{
+		if ((array[left] + array[right]) == targetSum)
+			return { array[left],array[right] };
+		else if ((array[left] + array[right]) < targetSum)
+			left++;
+		else if ((array[left] + array[right]) > targetSum)
+			right--;
+	}
+	return {};
+}

176efe1d7cb166474c00f49aafa61e8c61531ea7
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-12-09 14:06:07 +0530

    Update 4. Nth Fibonacci.cpp

--- 4. Nth Fibonacci.cpp
+++ 4. Nth Fibonacci.cpp
@@ -3,3 +3,14 @@ int getNthFib(int N)
 {
   return (pow((1 + sqrt(5)) / 2, N-1) - pow((1 - sqrt(5)) / 2, N-1)) / sqrt(5);
 }
+
+//TIME: O(2^N) , SPACE: O(N)
+int getNthFib(int N) 
+{
+	if(N==1)
+		return 0;
+	else if(N==2)
+		return 1;
+	else
+		return (getNthFib(N-1) + getNthFib(N-2));
+}
